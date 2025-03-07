45e2d9885b900b797186132de422977f626a678d
BigDogGoesWoof <1803023@uad.ac.uk>
2025-03-07 15:55:04 +0000

    FraserS - Started Work On Loading Next Phrase
    Some small changes to scene, creating new class with a dictionary of phrase to handle loading next scene based on gear

--- SpeedOfSound/Assets/Code/PhraseLoader.cs
+++ SpeedOfSound/Assets/Code/PhraseLoader.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class PhraseLoader : MonoBehaviour
 {
+
+    string phraseName;
     // Start is called before the first frame update
     void Start()
     {
+
         
+
+        var phraseDict = new Dictionary<string, GameObject>();
+        //get a reference to score script for gear value
+        //need a dictionary to access all of the various sections, so the next can be loaded with the correct difficulty
+        //might have to move to a system of the car moving forward, because moving the tracks when they are being changed might be challenging
     }
 
     // Update is called once per frame
